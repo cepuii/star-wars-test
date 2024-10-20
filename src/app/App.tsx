9364f1a65cee7e3e2b807d4cdab46bcdbdda1da9
cepuii <54589413+cepuii@users.noreply.github.com>
2024-10-20 12:37:29 +0300

    Add new components to main page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "../assets/react.svg";
 import Logo from "../components/ui/logo/logo";
+import StarsBackground from "../components/ui/starsBackground/starsBackground";
 import "./App.css";
 
 function App() {
@@ -8,24 +8,24 @@ function App() {
 
   return (
     <>
-      <div>
+      <StarsBackground />
+      <header>
         <Logo />
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      </header>
+      <section>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
     </>
   );
 }
